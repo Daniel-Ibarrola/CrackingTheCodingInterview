494111c4a2af56085e7997bd94c2825b2aded98c
Daniel-Ibarrola <dis96051@gmail.com>
2023-01-29 08:40:57 -0600

    Loop detection new algorithm has space complexity O(1).

--- chapter_2/loop_detection.cpp
+++ chapter_2/loop_detection.cpp
@@ -9,14 +9,28 @@ Node* detectLoop(Node* head)
 {
     // If the given linked list contains a loop, it returns
     // the node with the loop. Else, it returns a null pointer
-    std::unordered_set<Node*> visited;
-    while (head != nullptr)
-    {
-        if (visited.count(head))
-            return head;
-        visited.insert(head);
-        head = head->next;
+    Node* slow {head};
+    Node* fast {head};
 
+    // Find where pointers collide
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow)
+            break;
     }
-    return nullptr;
+
+    if (fast == nullptr || fast->next == nullptr)
+        return nullptr;
+
+    // Find beginning of the loop
+    slow = head;
+    while (slow != fast)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return slow;
 }
